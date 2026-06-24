f019a016e4aac33751c2ab857094cca308be4510
eyasinarafat3485-gif <eyasinarafat3485@gmail.com>
2026-06-24 19:31:19 +0600

    vercel env file added

--- index.js
+++ index.js
@@ -35,10 +35,14 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+
+client.connect(()=>{
+  console.log('connecting to db')
+}).catch(console.dir)
 
     const database = client.db("blood_Donation_db");
     const bloodRequestsCollection = database.collection("allBloods");
@@ -414,19 +418,15 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  }
-  finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-
-
+    // await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   }
+//   finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 
 app.listen(port, () => {
